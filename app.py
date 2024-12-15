b0eb84925dfc15ffc47f309fa87df9e357575b73
shu16 <hr.shu61@gmail.com>
2024-12-15 21:36:51 +0900

    ユーザー照合をカット

--- app.py
+++ app.py
@@ -63,15 +63,9 @@ def login():
     username = data.get('username')
     password = data.get('password')
 
-    # ユーザーをデータベースから取得
-    user = User.query.filter_by(username=username).first()
-
-    # ユーザーが存在し、パスワードが一致する場合
-    if user and check_password_hash(user.password, password):
-        access_token = create_access_token(identity=user.username)
-        return jsonify(access_token=access_token), 200
-
-    return jsonify({"message": "ユーザー名またはパスワードが正しくありません"}), 401
+    # 任意のメールアドレス・パスワードでaccess_tokenを発行
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token), 200
 
 # データベース初期化用
 @app.before_request
